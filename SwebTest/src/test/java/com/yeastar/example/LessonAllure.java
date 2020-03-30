32c4cf58ac9aa86e01e68fa9ce9e28708a7f419e
huangjx <huangjx6593>
2020-03-30 10:30:12 +0800

    add test demo

--- SwebTest/src/test/java/com/yeastar/example/LessonAllure.java
+++ SwebTest/src/test/java/com/yeastar/example/LessonAllure.java
@@ -4,10 +4,8 @@ package com.yeastar.example;
 
 import com.yeastar.untils.AllureReporterListener;
 import io.qameta.allure.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 
@@ -18,10 +16,24 @@ import static com.yeastar.swebtest.driver.SwebDriver.ys_apply;
  */
 @Listeners({AllureReporterListener.class})
 public class LessonAllure {
-    @BeforeClass
+    @BeforeClass @Step("[BeforeClass] Init test environment·····")
     public void BeforeClass() {
+    }
+
+    @BeforeMethod @Step("[BeforeMethod] Config test environment·····")
+    public void BeforeMethod() {
+    }
+
+
+    @BeforeMethod @Step("[AfterMethod] Restore  test environment·····")
+    public void AfterMethod() {
+    }
+
+    @AfterClass @Step("[AfterClass] Restore test class environment·····")
+    public void AfterClass() {
 
     }
+
     @Epic("Epic")
     @Feature("Feature")
     @Story("Story 1")
@@ -30,9 +42,28 @@ public class LessonAllure {
     @TmsLink("YD001")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void TestCase01() throws IOException {
-
+    public void TestCase01_Passed() throws IOException {
+        Methon_01();
+        Methon_02();
+        Methon_03();
+        Assert.assertTrue(true);
     }
+
+    @Epic("Epic")
+    @Feature("Feature")
+    @Story("Story 1")
+    @Description("Description")
+    @Issue("BUG_YD001")
+    @TmsLink("YD001")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void TestCase02_Failed() throws IOException {
+        Methon_01();
+        Methon_02();
+        Methon_03();
+        Assert.assertTrue(false);
+    }
+
     @Step("1.login pbx")
     public void Methon_01(){
 
@@ -47,5 +78,6 @@ public class LessonAllure {
     public void Methon_03(){
 
     }
+
 
 }
