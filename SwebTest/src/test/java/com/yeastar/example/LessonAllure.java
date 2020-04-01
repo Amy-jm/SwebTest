047d73bb71131f03a1c2ea3fbfc708b14a5c8592
huangjx <huangjx6593>
2020-04-01 10:08:59 +0800

    1.

--- SwebTest/src/test/java/com/yeastar/example/LessonAllure.java
+++ SwebTest/src/test/java/com/yeastar/example/LessonAllure.java
@@ -10,6 +10,7 @@ import org.testng.annotations.*;
 
 import java.io.IOException;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.close;
 import static com.yeastar.swebtest.driver.SwebDriver.ys_apply;
 
@@ -90,7 +91,7 @@ public class LessonAllure  extends SwebDriver{
 
     @Step("1.login pbx")
     public void Methon_01(){
-        login(LOGIN_USERNAME,LOGIN_PASSWORD);
+        login(LOGIN_USERNAME,LOGIN_PASSWORD,"english");
     }
 
     @Step("2.setting ")
@@ -100,6 +101,33 @@ public class LessonAllure  extends SwebDriver{
 
     @Step("3.assert ")
     public void Methon_03(){
+
+    }
+
+    /**
+     * 可选语言登录S系列设备
+     * @param username
+     * @param password
+     * @param language
+     */
+    public static void login(String username,String password,String language) {
+        if (language != null) { //不修改语言
+            if (language.equalsIgnoreCase("english")) { //修改语言，暂时就中文，英文
+                select(pageLogin.language, pageLogin.english);
+            } else {
+                select(pageLogin.language, pageLogin.chineseSimpleFied);
+            }
+        }
+        pageLogin.username.setValue(username);
+        pageLogin.password.setValue(password);
+        ys_waitingTime(1000);
+        pageLogin.login.click();
+
+        if(pageDeskTop.pp_comfirm.isDisplayed()){
+            setCheckBox(pageDeskTop.pp_agreement_checkBox,true);
+            pageDeskTop.pp_comfirm.click();
+        }
+        pageDeskTop.taskBar_User.should(exist);
 
     }
 
