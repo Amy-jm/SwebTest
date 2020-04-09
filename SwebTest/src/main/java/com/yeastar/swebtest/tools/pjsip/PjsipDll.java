e1922b06c82a5722015de88b5fa459168f0955f4
huangjx <huangjx6593>
2020-04-09 15:41:51 +0800

    1. add libpjsip

--- SwebTest/src/main/java/com/yeastar/swebtest/tools/pjsip/PjsipDll.java
+++ SwebTest/src/main/java/com/yeastar/swebtest/tools/pjsip/PjsipDll.java
@@ -44,7 +44,7 @@ public class PjsipDll {
          */
 
 
-        pjsipdll instance = (pjsipdll)Native.loadLibrary("home"+File.separator+"pjsip"+File.separator+"pjproject-2.6"+File.separator+"out"+File.separator+"lib"+File.separator+"pjsip",pjsipdll.class);
+        pjsipdll instance = (pjsipdll)Native.loadLibrary(File.separator+"home"+File.separator+"pjsip"+File.separator+"pjproject-2.6"+File.separator+"out"+File.separator+"lib"+File.separator+"pjsip",pjsipdll.class);
 
 
         //PjsipDll.dll的通用API
