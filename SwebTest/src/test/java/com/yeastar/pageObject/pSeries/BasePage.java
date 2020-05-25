cc65d03617e94ce61516e5934c3fc8b7b75414cf
huangjx <huangjx6593>
2020-05-25 13:26:30 +0800

    add page element

--- SwebTest/src/test/java/com/yeastar/pageObject/pSeries/BasePage.java
+++ SwebTest/src/test/java/com/yeastar/pageObject/pSeries/BasePage.java
@@ -5,17 +5,17 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class BasePage {
+public class BasePage implements IButton{
 
-    public  SelenideElement addBtn = $(By.xpath("//button//span[contains(text(),'Add')]"));
-    public  SelenideElement importBtn = $(By.xpath("//button//span[contains(text(),'Import')]"));
-    public  SelenideElement exportBtn = $(By.xpath("//button//span[contains(text(),'Export')]"));
-    public  SelenideElement deleteBtn = $(By.xpath("//button//span[contains(text(),'Delete')]"));
-    public  SelenideElement linkusServerBtn = $(By.xpath("//button//span[contains(text(),'LinkusServer')]"));
-    public  SelenideElement copyRoleBtn = $(By.xpath("//button//span[contains(text(),'Copy Role')]"));
-    public  SelenideElement saveBtn = $(By.xpath("//button//span[contains(text(),'Save')]"));
-    public  SelenideElement cancelBtn = $(By.xpath("//button//span[contains(text(),'Cancel')]"));
-    public  SelenideElement prefixBtn = $(By.xpath("//button//span[contains(text(),'Prefix')]"));
+//    public  SelenideElement addBtn = $(By.xpath("//button//span[contains(text(),'Add')]"));
+//    public  SelenideElement importBtn = $(By.xpath("//button//span[contains(text(),'Import')]"));
+//    public  SelenideElement exportBtn = $(By.xpath("//button//span[contains(text(),'Export')]"));
+//    public  SelenideElement deleteBtn = $(By.xpath("//button//span[contains(text(),'Delete')]"));
+//    public  SelenideElement linkusServerBtn = $(By.xpath("//button//span[contains(text(),'LinkusServer')]"));
+//    public  SelenideElement copyRoleBtn = $(By.xpath("//button//span[contains(text(),'Copy Role')]"));
+//    public  SelenideElement saveBtn = $(By.xpath("//button//span[contains(text(),'Save')]"));
+//    public  SelenideElement cancelBtn = $(By.xpath("//button//span[contains(text(),'Cancel')]"));
+//    public  SelenideElement prefixBtn = $(By.xpath("//button//span[contains(text(),'Prefix')]"));
 
     //button//span[contains(text(),'Import')]
 }
