f7d4725c64f5378d93e85ca0f8fc2a27dc2f30af
huangjx <huangjx6593>
2020-06-17 17:58:06 +0800

    add listeners for detail test case

---
+++ SwebTest/src/test/java/com/yeastar/testcase/pseries/TestExtensionSecurity.java
@@ -0,0 +1,10 @@
+package com.yeastar.testcase.pseries;
+
+/**
+ * @program: SwebTest
+ * @description: Extension security 用例
+ * @author: huangjx@yeastar.com
+ * @create: 2020/06/17
+ */
+public class TestExtensionSecurity {
+}
