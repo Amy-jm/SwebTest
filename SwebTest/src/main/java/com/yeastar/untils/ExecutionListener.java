d869220de1bae9eb5a22e565c101392102d020b7
huangjx <huangjx6593>
2020-07-14 17:58:57 +0800

    delete TestNGListenerP driver quite

--- SwebTest/src/main/java/com/yeastar/untils/ExecutionListener.java
+++ SwebTest/src/main/java/com/yeastar/untils/ExecutionListener.java
@@ -16,45 +16,24 @@ import static com.yeastar.controllers.WebDriverFactory.getDriver;
  * @create: 2020/06/23
  */
 @Log4j2
-public class ExecutionListener  extends TestListenerAdapter implements IInvokedMethodListener {
-    @Override
+public class ExecutionListener implements ITestListener {
+
     public void onTestStart(ITestResult iTestResult) {
         log.debug( "[ExecutionListener onTestStart] "+iTestResult.getTestClass()+"#"+iTestResult.getName());
-
     }
-   @Override
+
     public void onTestSuccess(ITestResult iTestResult) {
        log.debug( "[ExecutionListener Success] "+iTestResult.getTestClass()+"#"+iTestResult.getName());
-       try {
-           Cookie cookie = new Cookie("zaleniumTestPassed", "true");
-           getDriver().manage().addCookie(cookie);
-       }catch(java.lang.NullPointerException ex){
-           log.error("[ExecutionListener onTestSuccess add cookie to zalenium --> NullPointerException] {}",ex.getMessage());
-       }
        this.sendTestMethodStatus(iTestResult, "PASS");
     }
-    @Override
+
     public void onTestFailure(ITestResult iTestResult) {
         log.debug("[ExecutionListener Failure] "+iTestResult.getTestClass()+iTestResult.getName());
-        //更新用例状态 zalenium
-        try {
-            Cookie cookie = new Cookie("zaleniumTestPassed", "false");
-            getDriver().manage().addCookie(cookie);
-        }catch(java.lang.NullPointerException ex){
-            log.error("[ExecutionListener Failure add cookie to zalenium --> NullPointerException] {}",ex.getMessage());
-        }
         this.sendTestMethodStatus(iTestResult, "FAIL");
     }
-    @Override
+
     public void onTestSkipped(ITestResult iTestResult) {
         log.debug("[ExecutionListener SKIPPED] "+iTestResult.getTestClass()+iTestResult.getName());
-        //更新用例状态 zalenium
-        try {
-            Cookie cookie = new Cookie("zaleniumTestPassed", "false");
-            getDriver().manage().addCookie(cookie);
-        }catch(java.lang.NullPointerException ex){
-            log.error("[ExecutionListener Skipped add cookie to zalenium --> NullPointerException] {}",ex.getMessage());
-        }
         this.sendTestMethodStatus(iTestResult, "SKIPPED");
     }
 
@@ -97,15 +76,5 @@ public class ExecutionListener  extends TestListenerAdapter implements IInvokedM
         }catch(org.influxdb.InfluxDBIOException EX){
             log.error("[InfluxDB Server connection exception]");
         }
-    }
-
-    @Override
-    public void beforeInvocation(IInvokedMethod iInvokedMethod, ITestResult iTestResult) {
-
-    }
-
-    @Override
-    public void afterInvocation(IInvokedMethod iInvokedMethod, ITestResult iTestResult) {
-
     }
 }
