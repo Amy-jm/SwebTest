3c7f4825b8190bc6620b25e610eff076edfec927
huangjx <huangjx6593>
2020-08-21 14:21:55 +0800

    update testcase

--- SwebTest/src/test/java/com/yeastar/page/pseries/OperatorPanel/OperatorPanelPage.java
+++ SwebTest/src/test/java/com/yeastar/page/pseries/OperatorPanel/OperatorPanelPage.java
@@ -174,6 +174,8 @@ public class OperatorPanelPage extends BasePage {
      * @return
      */
     public  OperatorPanelPage rightTableAction(TABLE_TYPE tableType,String tagName,RIGHT_EVENT event,String eventParam,boolean isClickVoiceMailImage){
+        refresh();//todo  for linux 必须刷新后才能进行邮件操作 chrome 84
+        sleep(WaitUntils.SHORT_WAIT);
         if(tableType==TABLE_TYPE.INBOUND){
             actions().contextClick(WebDriverFactory.getDriver().findElement(By.xpath(String.format(TABLE_INBOUND_XPATH+"/tbody//td[contains(text(),\"%s\")]",tagName)))).perform();
         }
@@ -190,6 +192,8 @@ public class OperatorPanelPage extends BasePage {
      * @param tagName
      */
     public  OperatorPanelPage rightTableAction(TABLE_TYPE tableType,String tagName){
+        refresh();//todo  for linux 必须刷新后才能进行邮件操作 chrome 84
+        sleep(WaitUntils.SHORT_WAIT);
         if(tableType==TABLE_TYPE.INBOUND){
             actions().contextClick(WebDriverFactory.getDriver().findElement(By.xpath(String.format(TABLE_INBOUND_XPATH+"/tbody//td[contains(text(),\"%s\")]",tagName)))).perform();
         }
