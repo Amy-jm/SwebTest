9fc19571cf9ddd0763d8e68d10c8ca65a0af6202
huangjx <huangjx6593>
2020-09-08 16:29:00 +0800

    debug browser log for api

--- SwebTest/src/test/java/com/yeastar/testcase/pseries/TestDebug.java
+++ SwebTest/src/test/java/com/yeastar/testcase/pseries/TestDebug.java
@@ -1,28 +1,27 @@
 package com.yeastar.testcase.pseries;
 
-import com.codeborne.selenide.Condition;
-import com.yeastar.page.pseries.HomePage;
 import com.yeastar.page.pseries.TestCaseBase;
-import com.yeastar.untils.*;
-import io.qameta.allure.*;
+import com.yeastar.untils.AllureReporterListener;
+import com.yeastar.untils.ExecutionListener;
+import com.yeastar.untils.TestNGListenerP;
+import com.yeastar.untils.WaitUntils;
 import lombok.extern.log4j.Log4j2;
-import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
 
-@Listeners({ExecutionListener.class,AllureReporterListener.class, TestNGListenerP.class})
+@Listeners({ExecutionListener.class, AllureReporterListener.class, TestNGListenerP.class})
 @Log4j2
 public class TestDebug extends TestCaseBase {
 
 
     @Test(invocationCount = 50)
-    public void testDebugLogin(){
+    public void testDebugLogin() {
         step("1:login PBX");
-        auto.loginPage().login("0",EXTENSION_PASSWORD_NEW);
-        sleep(WaitUntils.SHORT_WAIT*2);
+        auto.loginPage().login("0", EXTENSION_PASSWORD_NEW);
+        sleep(WaitUntils.SHORT_WAIT * 2);
     }
 }
 
