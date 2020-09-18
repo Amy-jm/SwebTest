5dc76cdf49c9a2d5466d501ac615b8726a9c5cc0
huangjx <huangjx6593>
2020-09-18 08:51:31 +0800

    debug

--- SwebTest/src/test/java/com/yeastar/page/pseries/TestCaseBase.java
+++ SwebTest/src/test/java/com/yeastar/page/pseries/TestCaseBase.java
@@ -50,7 +50,7 @@ public class TestCaseBase extends BaseMethod {
         auto = PageEngine.getInstance();
         try {
             pjsip = new PjsipApp();
-            pjsip.work(454545454545l);
+//            pjsip.work(454545454545l);
             log.debug("【pjsip new】 "+pjsip);
         } catch (Throwable ex) {
          log.error("【PjsipException new】" + ex);
